e76861608b68971f3f7fca850e05fb29ba751e9d
Eirik Reiestad <eirik@reiestad.no>
2023-09-23 00:17:11 +0200

    part2

--- assignment2/src/part2.py
+++ assignment2/src/part2.py
@@ -1,5 +1,6 @@
 """This is the code for Part2"""
 import datetime
+import math
 from DbConnector import DbConnector
 
 class DbExecutor:
@@ -67,21 +68,64 @@ class DbExecutor:
                 result.append((user[0], user[3], user[2]-user[1]))
         return (result, True)
     
-    def get_activity_distance(self):
+    def get_activity_distance(self) -> (int, int):
         """
         Return:
         """
+        # First, get the longest distance
+        # This is porly written and should be revised
         query = """
-            SELECT a.transportation_mode 
+            SELECT a.id, tp.lat, tp.lon, a.transportation_mode, u.id
             FROM Activity a
-            INNER JOIN TrackPoint tp
-            WHERE 
+            INNER JOIN TrackPoint tp ON a.id = tp.activity_id 
+            INNER JOIN User u ON u.id = a.user_id
+            WHERE tp.date_time >= a.start_date_time
+            AND tp.date_time <= a.end_date_time
         """
+
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+
+        activity = {}
+        last_activity_position = {}
+        transportation = {}
+        activity_user = {}
+
+        for res in result:
+            activity_id = res[0]
+            lat = res[1]
+            lon = res[2]
+
+
+            last_lat, last_lon = last_activity_position.get(activity_id, (lat, lon))
+            last_activity_position[activity_id] = (lat, lon)
+            distance = math.sqrt((lat - last_lat)**2 + (lon - last_lon)**2)
+            activity[activity_id] = activity.get(activity_id, 0) + distance
+            transportation[activity_id] = res[3]
+            activity_user[activity_id] = res[4]
+
+        longest_activity = {}
+        for act in activity.items():
+            transportation_mode = transportation[act[0]]
+            longest_activity[transportation_mode] = (
+                act[0],
+                max(act[1], longest_activity.get(transportation_mode, (0, 0))[1]))
+
+        result = []
+        for longest in longest_activity.items():
+            activity_id = longest[1][0]
+            transportation_mode = longest[0]
+            user_id = activity_user[activity_id] 
+            result.append((user_id, transportation_mode))
+
+        return result
 
 
 if "__main__" == __name__:
     connector = DbConnector()
     executor = DbExecutor()
 
+    res = executor.get_activity_distance()
+    print(res)
 
     connector.close_connection()
