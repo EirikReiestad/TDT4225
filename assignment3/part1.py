945deab79b7fc0aebd646d3f9cb8f72c307d51c2
henriklarsen28 <henrik.hl@hotmail.com>
2023-10-08 19:23:00 +0200

    feat: loaded some into the database, still missing some data, and change id to integer

--- assignment3/part1.py
+++ assignment3/part1.py
@@ -33,14 +33,6 @@ class Assignement3:
         collection = self.db["TrackPoint"]
         collection.drop()
 
-    def insert_User(self, id: str, hasLabels: bool):
-        collection = self.db["User"]
-        collection.insert_one({"_id": id, "hasLabels": hasLabels})
-
-    def insert_ActivityMany(self, activities: list):
-        collection = self.db["Activity"]
-        collection.insert_many(activities)
-
     def insertData(self):
         self.drop_User()
         self.drop_Activity()
@@ -63,11 +55,15 @@ class Assignement3:
         folders = os.listdir(directory_path)
 
         for userId in folders:
-            if userId in user_label:
 
-                # Read from labels.txt and insert into Activity table
+            if userId in user_label:
+                # Insert the user into User table
+                collection = self.db["User"]
+                collection.insert_one({"_id": userId, "hasLabels": True})
+
+                # Insert the activitues with labels into Activity table
                 with open(
-                    "./dataset/Data/" + userId + "/labels.txt", "r", encoding="UTF-8"
+                        "./dataset/Data/" + userId + "/labels.txt", "r", encoding="UTF-8"
                 ) as labels:
                     values = []
                     labels.readline()
@@ -77,16 +73,107 @@ class Assignement3:
                         start_time = line[0].replace("/", "-")
                         end_time = line[1].replace("/", "-")
                         transportation_mode = line[2]
-                        values.append({"user_id": userId, "transportation_mode": transportation_mode, "start_time": start_time, "end_time": end_time})
-                    self.insert_ActivityMany(values)
+                        values.append(
+                            ({"user_id": userId,"transportation_mode": transportation_mode, "start_time": start_time, "end_time": end_time})
+                        )
 
-                self.insert_User(userId, True)
+                    # Insert all labeled activites at once
+                    collection = self.db["Activity"]
+                    collection.insert_many(values)
+
+                # Retrieve the activity with the exact match if exists
+                # If more than one exists, then order by DESC for later use case
+                collection = self.db["Activity"]
+                activity_id = collection.find({"user_id": userId, "start_time": start_time, "end_time": end_time})
+
+                # Extracts the id from the result
+                activity_id = activity_id[0]["_id"]
 
 
+                # If there is no match, then this file of trackpoint is skipped
+                # Because user who saves transportation mode is not allowed
+                # To have transportation mode equals to NULL
+                # Thus, we cannot add a new Activity row for it
+                if not activity_id:
+                    continue
 
             else:
-                print(userId)
-                self.insert_User(userId, False)
+
+                print("User", userId)
+                collection = self.db["User"]
+                collection.insert_one({"_id": userId, "hasLabels": False})
+
+
+            # Retrieve files in current user's Trajectory folder
+            # Each file corresponds to an activity
+            trajectory_path = "./dataset/Data/" + userId + "/Trajectory/"
+            files = os.listdir(trajectory_path)
+
+            for file in files:
+                # Read the file
+                with open(trajectory_path + file, "r", encoding="UTF-8") as f:
+                    lines = f.readlines()
+
+                # Process the file to retrieve start and end time
+                # Start time: Date of the first trackpoint
+                # End time: Date of the last trackpoint
+                start_time = lines[6].split(",")[5] + " " + lines[6].split(",")[6]
+                end_time = lines[-1].split(",")[5] + " " + lines[-1].split(",")[6]
+
+                # If user saves transportation mode, then retrieve the activity in
+                # Activity table which has exact matches on starttime and end time
+            # If file includes more than 2500 (+6 to count for headers) trackpoints
+            if len(lines) > 2500 + 6:
+                # If user saves transportation mode, delete the activity that this
+                # file belongs to as it will not have any trackpoints, thus is not relevant
+                if userId in user_label:
+                    collection = self.db["Activity"]
+                    collection.delete_many({"user_id": userId})
+
+                # Thereafter, skip this file
+                continue
+
+
+            # If the user saves transportation mode, all activities by the user has already been added
+            # We choose the most recent added activity by using the ORDER clause above with DESC
+            # This is for example if a user starts driving the car, but drives slow in the beginning
+            # it might have been perceived as walking in the beginning and added to the Activity list
+            # after the user speeds up, a new activity is added with the same endtime and starttime,
+            # but with another transportation mode
+            if userId in user_label:
+                activity_id = activity_id
+            else:
+                # If user does not save transportation, it means no activity has been added
+                # and therefore we add a new activity with transportation_mode = None (NULL)
+                activity_id = self.db["Activity"].insert_one({"user_id": userId, "transportation_mode": None, "start_date_time": start_time, "end_date_time": end_time}).inserted_id
+
+            values = []
+            # Then process each line of the activity file, but skip the headers (first 6 lines)
+            for line in lines[6:]:
+                line = line.strip().split(",")
+                lat = line[0]
+                lon = line[1]
+                altitude = line[3]
+                date_days = line[4]
+                date_time = line[5] + " " + line[6]
+                values.append(
+                    ({"activity_id": activity_id, "lat": lat, "lon": lon, "altitude": altitude, "date_dats": date_days,"date_time": date_time})
+                )
+
+            # insert the trackpoints as a batch to be more efficient
+            collection = self.db["TrackPoint"]
+            ids = collection.insert_many(values)
+
+            # Insert the ids of the activities into the user
+            collection = self.db["User"]
+            collection.update_one({"_id": userId}, {"$set": {"activities": ids.inserted_ids}})
+
+            # Insert the ids of the trackpoints into the activity
+            collection = self.db["Activity"]
+            collection.update_one({"_id": activity_id}, {"$set": {"trackpoints": ids.inserted_ids}})
+
+
+
 
 def main():
     assignment3 = Assignement3()
